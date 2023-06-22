afba047596ffd3a133dfb24d9f97587de01568ac
Chalapathi <126261121+ChalapathiDsp1@users.noreply.github.com>
2023-06-22 21:14:14 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
 region = "ap-south-1"
-access_key = ""
-secret_key = ""
+access_key = "AKIAW7UULS6PJ7BDQS4C"
+secret_key = "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"
 }
 
 resource "aws_instance" "key" {
-ami = "ami-012b9156f755804f5"
+ami = "ami-090e0fc566929d98b"
 instance_type = "t2.micro"
-key_name = "nwkpp"
+key_name = "VIGINIA"
 count = 1
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
